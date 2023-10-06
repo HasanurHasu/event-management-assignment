25204f199092a77da595f962ce2fdefc3c1cf6fa
Md. Hasanur Rahman <hasanurhasu121@gmail.com>
2023-10-07 00:15:18 +0600

    added Navbar

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,9 @@
+import Navbar from "../Navbar/Navbar";
 
 const Home = () => {
     return (
         <div>
-            <h1>This is home page</h1>
+            <Navbar></Navbar>
         </div>
     );
 };
