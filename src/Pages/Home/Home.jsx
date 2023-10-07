932b0fc42609d4147612817bb76965cc74adaefe
Md. Hasanur Rahman <hasanurhasu127@gmail.com>
2023-10-07 14:03:32 +0600

    create JSON file

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,13 @@
+import Banner from "../Banner/Banner";
 import Navbar from "../Navbar/Navbar";
+import Services from "../Services/Services";
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Banner></Banner>
+            <Services></Services>
         </div>
     );
 };
---
+++ src/Pages/Services/Services.jsx
@@ -0,0 +1,11 @@
+
+const Services = () => {
+    return (
+        <div>
+            <h1 className="text-4xl font-extrabold text-center my-6">Our Services</h1>
+            <p className="text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci accusantium, unde illo, quas dignissimos magnam saepe odio consequatur excepturi voluptatem similique maiores, neque aliquid ut.</p>
+        </div>
+    );
+};
+
+export default Services;
