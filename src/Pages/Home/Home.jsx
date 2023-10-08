d8a1802419fe57fb56fd6c2236a78ded81c4add8
Md. Hasanur Rahman <hasanurhasu121@gmail.com>
2023-10-09 00:06:06 +0600

    added extra section Subscribe

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../Banner/Banner";
 import Services from "../Services/Services";
+import Subscribe from "../Subscribe/Subscribe";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Services></Services>
+            <Subscribe></Subscribe>
         </div>
     );
 };
---
+++ src/Pages/Subscribe/Subscribe.jsx
@@ -0,0 +1,16 @@
+
+const Subscribe = () => {
+    return (
+        <div className="my-10 flex flex-col items-center gap-4 py-20 bg-slate-400">
+            <h1 className="text-3xl font-bold text-black">Subscribe</h1>
+            <hr className="border-t-2 border-black bg-black w-1/6" />
+            <p className="text-black">Subscribe our newsletter to stay updated every moment</p>
+            <div className="flex">
+                <input type="text" placeholder="Enter a valid email address" className="border-2 rounded-l-md w-96 py-2 px-6" />
+                <button className="bg-[#D72050] px-5 py-2 rounded-r-md text-white ">Submit</button>
+            </div>
+        </div>
+    );
+};
+
+export default Subscribe;
