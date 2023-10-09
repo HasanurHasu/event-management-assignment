b2bd44f77790273a07a953a69d4da0d5580dd93c
Md. Hasanur Rahman <hasanurhasu127@gmail.com>
2023-10-09 18:10:05 +0600

    added aos

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -1,7 +1,7 @@
-
+import 'aos/dist/aos.css'
 const Footer = () => {
     return (
-        <footer className="footer footer-center p-10 py-20 bg-neutral  text-neutral-content rounded">
+        <footer data-aos="fade-up" className="footer footer-center p-10 py-20 bg-neutral  text-neutral-content rounded">
             <nav className="grid grid-flow-col gap-4">
                 <a className="link link-hover">About us</a>
                 <a className="link link-hover">Contact</a>
--- src/Pages/TeamMember/TeamMemberCard.jsx
+++ src/Pages/TeamMember/TeamMemberCard.jsx
@@ -1,17 +1,24 @@
 
 import PropTypes from 'prop-types';
+import 'aos/dist/aos.css'
+import Aos from "aos";
+import { useEffect } from 'react';
 
-const TeamMemberCard = ({member}) => {
-    const {name, img, designation} = member;
+const TeamMemberCard = ({ member }) => {
+    useEffect(() => {
+        Aos.init({ duration: '1000', delay: '500' })
+    }, [])
+
+    const { name, img, designation } = member;
     return (
-       <div>
-         <div style={{backgroundImage: `url(${img})`, backgroundPosition: 'center top', backgroundSize: 'cover'}} className='h-80 w-full rounded-md flex items-end'>
-            <div className='bg-[#D72050] w-full py-2 px-4 text-white rounded-b-md'>
-                <h1 className='text-xl font-medium'>{name}</h1>
-                <p className='text-sm font-light'>{designation}</p>
+        <div data-aos="fade-up">
+            <div style={{ backgroundImage: `url(${img})`, backgroundPosition: 'center top', backgroundSize: 'cover' }} className='h-80 w-full rounded-md flex items-end'>
+                <div className='bg-[#D72050] w-full py-2 px-4 text-white rounded-b-md'>
+                    <h1 className='text-xl font-medium'>{name}</h1>
+                    <p className='text-sm font-light'>{designation}</p>
+                </div>
             </div>
         </div>
-       </div>
     );
 };
 
